42a1eb14cd8b130b7060f0829b664562d6ab1d30
rbozek <rbozek@gmail.com>
2023-12-23 20:41:25 -0500

    add placeholder functions

--- js/app.js
+++ js/app.js
@@ -13,12 +13,29 @@ let playerScore, currentQuestion, quizOver, letPerfectScore
 
 
 /*------------ Functions ------------*/
+function init(){
+  render()
+}
+// init()
+
+function render(){ 
+}
+
+function updateBoard(){
+}
+
+function updateScore(){
+}
+
+
+
+
 
 // QUESTIONS
 // ** container vs div?
 
 // PSEUDOCODE
-// 1) Define the required variables used to track the state of the game - GAME STATE
+// // 1) Define the required variables used to track the state of the game - GAME STATE
 // // -scorecard / player score - let playerScore 
 // // -which question player is on - let currentQuestion
 // // -final score / quiz complete - let quizOver (??Maybe not? Since there is no “win state”, only a finite amount of questions)
@@ -30,16 +47,15 @@ let playerScore, currentQuestion, quizOver, letPerfectScore
 // 		-(points deducted for incorrect answers?)
 // 	-separate file for data (prompts/answers) (is this cached element reference?)
 
-// 3) Upon loading, the game state should be initialized, and a function should be 
-//    called to render this game state
-// 	-init function - reset to beginning welcome page, reset score to 0
-// 		-render function at end of init
+// 3) Upon loading, the game state should be initialized, and a function should be called to render this game state
+// // 	-init function - reset to beginning welcome page, reset score to 0
+// // 		-render function at end of init
 // 		-(possible to choose next category without resetting to the welcome page? I.e. continue onto another category from results page? Or too complex for me at the moment?)
 // 	-player will choose category, then hit “begin” button
 
 // 4) The state of the game should be rendered to the user
-// 	-updateBoard (to display each new question)
-// 	-updateScore (after each question)
+// // 	-updateBoard (to display each new question)
+// // 	-updateScore (after each question)
 // 	-timer?? Resets for each question, or for entire category (does this belong here?)
 // 	-quizOver - final state, displays score, choices of other categories to try
 
