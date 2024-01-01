950b7638699658666c3c878c51ba39749f89a599
rbozek <rbozek@gmail.com>
2024-01-01 12:45:03 -0500

    commit changes that i forgot to push during past attempts. clean up file structure. create questions file for Q&A storage. update cached element refs. create empty array questions variable. add function & export to questions data file. develop basic functions createQuestion, render. create reset button & set up empty reset function. fill in init function. work on createQuestion & render but still confused, in conjunction with printQuestion. get rid of unneccesary render fxn.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,85 @@
 console.log('test!');
 
 /*------------ Constants ------------*/
+// import { getNextQuestion } from "../js/questions.js"
+import { questions } from "../js/questions.js"
 
 
 /*------------ Variables ------------*/
 let playerScore, currentQuestion, quizOver, letPerfectScore
+// const questions = []
 
 /*---- Cached Element References ----*/
-
+const btnCategory1 = document.querySelector("#btn-category-1")
+// const btnCategory2 = document.querySelector("#btn-category-2")
+// const btnCategory3 = document.querySelector("#btn-category-3")
+const questionContainer = document.querySelector
+("#question-container")
+const btnReset = document.querySelector
+("#btn-reset")
 
 /*--------- Event Listeners ---------*/
+btnCategory1.addEventListener('click', createQuestion)
+btnReset.addEventListener('click', reset)
 
 
 /*------------ Functions ------------*/
+function createQuestion(){
+  // const newQuestion = getNextQuestion()
+  // console.log(newQuestion.toString());
+  // getNextQuestion()
+  console.log('createQuestion fxn');
+  // console.log(getNextQuestion())
+  questionContainer.innerHTML = ''
+  console.log('render fxn works')
+  questions.forEach((ques,ans1,ans2,ans3,ans4) => {
+    printQuestion(ques,ans1,ans2,ans3,ans4)
+  })
+  // render()
+}
+
+// function render(){ 
+//   questionContainer.innerHTML = ''
+//   // questionContainer.innerHTML = 'render fxn'
+//   console.log('render fxn works')
+//   // console.log(getNextQuestion())
+//   questions.forEach((ques,ans1,ans2,ans3,ans4) => {
+//     printQuestion(ques,ans1,ans2,ans3,ans4)
+//   })
+// }
+
+function printQuestion(ques,ans1,ans2,ans3,ans4){
+  console.log('printQuestion works')
+  // console.log(ques,ans1,ans2,ans3,ans4)
+  let questionDiv = document.createElement('div')
+  questionDiv.className = `question-div`
+  questionDiv.innerHTML = // `test inside questionDiv`
+  `<div>
+  <p>${ques}</p>
+  <p>${ans1}</p>
+  <p>${ans2}</p>
+  </div>`
+  console.log('printQuestion works')
+  questionContainer.appendChild(questionDiv)
+}
+
+// const singleQuestion = questions.map(){
+//   const transformedQuestion = { name: student, studentNum: index + 1, enrolled: true }
+//   return transformedQuestion
+// }
+
+
 function init(){
-  render()
+  playerScore = null
+  currentQuestion = null
+  quizOver = false
+  letPerfectScore = false
+  // render()
 }
 // init()
 
-function render(){ 
+function reset(){
+  init()
 }
 
 function updateBoard(){
@@ -30,7 +91,11 @@ function updateScore(){
 
 
 //next:
+//get questions to print out in HTML
+//how to proceed to next object in arrya for questions
 //click category, make tagline, rules, and category buttons disappear
+
+
 
 // QUESTIONS
 // ** container vs div?
@@ -43,10 +108,10 @@ function updateScore(){
 // // -(maybe separate action for PERFECT score - letPerfectScore)
 
 // 2) Store cached element references
-// 	-container for question & answers
+//	// -container for question & answers
 // 	-multiple-choice buttons - eliminated until correct answer is selected 
 // 		-(points deducted for incorrect answers?)
-// 	-separate file for data (prompts/answers) (is this cached element reference?)
+// // 	-separate file for data (prompts/answers) - NOT cached element - it's a separate data file
 
 // 3) Upon loading, the game state should be initialized, and a function should be called to render this game state
 // // 	-init function - reset to beginning welcome page, reset score to 0
@@ -60,11 +125,12 @@ function updateScore(){
 // 	-timer?? Resets for each question, or for entire category (does this belong here?)
 // 	-quizOver - final state, displays score, choices of other categories to try
 
+
 // 5) Define the required constants
-// 	-correct answer for each quiz question
+// // 	-correct answer for each quiz question
 
 // 6) Event listeners
-// 	-handleClick with evt parameter
+//   	-handleClick with evt parameter
 // 		-play sound when selecting choices (diff sound for incorrect/correct)
 // 		-(ICEBOX) deduct points for incorrect answer
 // 		-add points & move to next question upon correct answer	
---
+++ js/questions.js
@@ -0,0 +1,18 @@
+const questions = [
+  {ques: "What do you do etc etc?", ans1: "be rude", ans2: "be nice", ans3: "be crude", ans4: "be tude"},
+  {ques: "Question #2?", ansCorrect: "help people", ansWrong: "hurt people", ansWrong: "kill people", ansWrong: "steal someones car"},
+  {ques: "Question #3?", ansWrong: "bad friend", ansWrong: "no friend", ansWrong: "donut stealer", ansCorrect: "Good friend"},
+]
+
+export {
+  questions
+}
+
+
+// function getNextQuestion() {
+//   return questions[0]
+// }
+
+// export {
+//   getNextQuestion,
+// }
